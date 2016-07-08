30b18e9f20d634600b056852c3721ae0fdf755f4
apple95 <rajaa@roe-hulman.edu>
2016-07-09 01:33:23 +0530

    Implemented Find A DOC and created a layout

--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/MainActivity.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/MainActivity.java
@@ -1,13 +1,23 @@
 package com.ashokrv95gmail.csse483androidfinalproject;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,15 +27,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Button button = (Button) findViewById(R.id.button_view);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                DialogFragment df = new DialogFragment(){
+                    @NonNull
+                    @Override
+                    public Dialog onCreateDialog(Bundle savedInstanceState){
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        View view = getActivity().getLayoutInflater().inflate(R.layout.hospital_view, null);
+                        builder.setView(view);
+                        builder.setTitle("HOSPITALS");
+                        // boolean checked = ((RadioButton) view).isChecked();
+
+                        builder.setPositiveButton(android.R.string.ok, null);
+
+
+
+
+
+
+                        builder.setNegativeButton(android.R.string.cancel,null);
+
+                        return builder.create();
+                    }
+                };
+                df.show(getSupportFragmentManager(),"select");
+
             }
         });
+
+
+    }
+
+    private void findDoc() {
+
     }
 
     @Override
