3f894b0736418495408ab471c5be681620f273aa
apple95 <rajaa@roe-hulman.edu>
2016-07-23 10:00:42 +0530

    Terre Haute Hospital list added

--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity3Hospital.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity3Hospital.java
@@ -1,18 +1,20 @@
 package com.ashokrv95gmail.csse483androidfinalproject;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.ListView;
 
 /**
  * Created by ashokvardhanraja on 7/23/16.
  */
 public class Activity3Hospital extends Activity {
+    private NameAdapterTerreHaute mAdapter;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main3);
+        ListView listView = (ListView)findViewById(R.id.list_view);
+        mAdapter = new NameAdapterTerreHaute(this);
+        listView.setAdapter(mAdapter);
 
     }
 
---
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/NameAdapterTerreHaute.java
@@ -0,0 +1,93 @@
+package com.ashokrv95gmail.csse483androidfinalproject;
+
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by ashokvardhanraja on 6/20/16.
+ */
+public class NameAdapterTerreHaute extends BaseAdapter {
+    private Context mContext;
+    final ArrayList<String> mNames = new ArrayList<>();
+    private Random mRandom = new Random();
+
+
+    public NameAdapterTerreHaute(Context context){
+        mContext = context;
+        for(int i =0;i<2;i++){
+            mNames.add(getRandomName(i));
+        }
+    }
+
+    private String getRandomName(int pos){
+        String[] names = new String[]{
+                "UnionHospital", "TerreHaute GeneralHospital"
+
+        };
+        int count = names.length;
+        return names[pos];
+    }
+
+
+    @Override
+    public int getCount() {
+        return mNames.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = null;
+        if(convertView == null){
+            //Make a view
+            view = LayoutInflater.from(mContext).inflate(R.layout.hospital_view,parent,false);
+        }else{
+            view = convertView;
+        }
+        //Customize it
+        String name = mNames.get(position);
+        TextView positionTextView = (TextView)view.findViewById(R.id.position_view);
+
+        switch(name) {
+            case "UnionHospital":
+                positionTextView.setText(name);
+                break;
+
+            case "TerreHaute GeneralHospital":
+                positionTextView.setText(name);
+                break;
+            default:
+                break;
+        }
+
+        return view;
+
+
+    }
+    public void addName(int pos){
+
+        mNames.add(0,getRandomName(pos));
+        notifyDataSetChanged();
+    }
+
+}
