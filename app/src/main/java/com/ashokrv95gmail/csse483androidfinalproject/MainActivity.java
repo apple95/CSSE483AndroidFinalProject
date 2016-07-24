252e9909e59ed6a88b01f311a8cfb58129024951
vinnyaa <andersva@rose-hulman.edu>
2016-07-24 18:46:21 -0400

    Log in button
    Log in item on the toolbar main page. If the password entered is
    "password" a toast pops up that says "Tried to log in as: " + the
    username entered

--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/MainActivity.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ashokrv95gmail.csse483androidfinalproject;
 
+import android.app.Application;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -21,9 +22,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String name;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         final Button button = (Button) findViewById(R.id.button_view);
-        Button button1 = (Button)findViewById(R.id.buttonemergency_view);
-        Button button2 = (Button)findViewById(R.id.buttonfixappointment_view);
+        Button button1 = (Button) findViewById(R.id.buttonemergency_view);
+        Button button2 = (Button) findViewById(R.id.buttonfixappointment_view);
         Button button3 = (Button) findViewById(R.id.buttonpay_view);
 
 
@@ -41,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(v.getContext(), Activity2Hospital.class);
-                startActivityForResult(myIntent,1);
+                startActivityForResult(myIntent, 1);
 
 
             }
@@ -50,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(v.getContext(), Activity2Emergency.class);
-                startActivityForResult(myIntent,1);
+                startActivityForResult(myIntent, 1);
 
 
             }
@@ -61,12 +64,10 @@ public class MainActivity extends AppCompatActivity {
 
 
                 Intent myIntent = new Intent(v.getContext(), Activity2FixAppointment.class);
-                startActivityForResult(myIntent,1);
+                startActivityForResult(myIntent, 1);
 
 
-           }
-
-
+            }
 
 
         });
@@ -74,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(v.getContext(), Activity2PayBill.class);
-                startActivityForResult(myIntent,1);
+                startActivityForResult(myIntent, 1);
             }
         });
 
@@ -93,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -102,6 +103,42 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.log_in_item) {
+            DialogFragment df = new DialogFragment() {
+                @NonNull
+                @Override
+                public Dialog onCreateDialog(Bundle savedInstanceState) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    final View view = getActivity().getLayoutInflater().inflate(R.layout.login, null);
+                    builder.setView(view);
+                    // capture
+                    final EditText nameEditText = (EditText) view.findViewById(R.id.log_in_username);
+                    final EditText passwordEditText = (EditText) view.findViewById(R.id.log_in_password);
+
+
+                    //buttons
+                    builder.setNegativeButton(android.R.string.cancel, null);
+                    builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (!nameEditText.getText().toString().isEmpty()) {
+                                name = nameEditText.getText().toString();
+                            }
+                            if (!passwordEditText.getText().toString().isEmpty()) {
+                                password = passwordEditText.getText().toString();
+                            }
+                            if(passwordEditText.getText().toString().equals("password")) {
+                                Log.d("TTT", "onClick: password checks out");
+                                Toast.makeText(getContext(), "Tried to login as: " + name, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                    return builder.create();
+                }
+
+            };
+            df.show(getSupportFragmentManager(), "add");
         }
 
         return super.onOptionsItemSelected(item);
