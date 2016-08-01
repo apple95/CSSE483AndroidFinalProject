5902546b04b84ead480bc34aa2c9b3b35bc8fa31
apple95 <rajaa@roe-hulman.edu>
2016-08-01 12:06:16 +0530

    Adding phone call basics for emergency

--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity2Emergency.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity2Emergency.java
@@ -1,8 +1,12 @@
 package com.ashokrv95gmail.csse483androidfinalproject;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,10 +17,16 @@ import android.widget.EditText;
 public class Activity2Emergency extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.emergency_view);
-
-
-
+        setContentView(R.layout.phone_call);
+        Button button = (Button) findViewById(R.id.Callphone);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:0377778888"));
+               // startActivityForResult(callIntent, 1);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/AllergyDoc.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/AllergyDoc.java
@@ -11,7 +11,6 @@ public class AllergyDoc extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.docdetail);
 
-
     }
 
 }
