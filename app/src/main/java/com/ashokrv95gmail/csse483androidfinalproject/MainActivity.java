cda06d8ac2e2dbba99dbb63a5e1b45be703a959d
vinnyaa <andersva@rose-hulman.edu>
2016-08-02 17:59:17 -0400

    Firebase additions
    log in now supported from login toolbar, c@d.com with password: dddddd
    just like the class demo. Also, there is a user: user@name.com with
    password: password
    
    --coming soon: logout functionality with a toggle on the toolbar from
    login to logout

--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/MainActivity.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,9 +25,19 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     private String name;
     private String password;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private OnCompleteListener mOnCompleteListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+        initializeListeners();
+
         final Button button = (Button) findViewById(R.id.button_view);
         Button button1 = (Button) findViewById(R.id.buttonemergency_view);
         Button button2 = (Button) findViewById(R.id.buttonfixappointment_view);
@@ -135,6 +150,8 @@ public class MainActivity extends AppCompatActivity {
                                 Log.d("TTT", "onClick: password checks out");
                                 Toast.makeText(getContext(), "Tried to login as: " + name, Toast.LENGTH_SHORT).show();
                             }
+
+                            emailLogIn();
                         }
                     });
 
@@ -146,5 +163,51 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    protected void emailLogIn() {
+        if (!(name.length() == 0 || name == null)) {
+            mAuth.signInWithEmailAndPassword(name,password).addOnCompleteListener(mOnCompleteListener);
+            FirebaseUser user = mAuth.getCurrentUser();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mAuthListener != null && mAuth != null) {
+            mAuth.addAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null && mAuth != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void initializeListeners() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null) {
+                    Log.d("TTT", "onAuthStateChanged: user is: " + user.getUid());
+                } else {
+                    Log.d("TTT", "onAuthStateChanged: user is null when adding Authstatelistener");
+                }
+            }
+        };
+        mOnCompleteListener = new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if(!task.isSuccessful()) {
+                    Log.d("TTT", "onComplete, user has logged in.");
+                }
+            }
+        };
     }
 }
