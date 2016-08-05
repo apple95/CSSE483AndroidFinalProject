1ab5de8d0f7e9cdb9f4e3a2282e13f311d32e35d
apple95 <rajaa@roe-hulman.edu>
2016-08-05 19:20:52 +0530

    Phone call for emergency implemented

--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity2Emergency.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity2Emergency.java
@@ -2,11 +2,15 @@ package com.ashokrv95gmail.csse483androidfinalproject;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,18 +19,25 @@ import android.widget.EditText;
  * Created by ashokvardhanraja on 7/23/16.
  */
 public class Activity2Emergency extends Activity {
+    EditText editText;
+    Button button;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.phone_call);
         Button button = (Button) findViewById(R.id.Callphone);
+
+        editText=(EditText)findViewById(R.id.changeText);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String number=editText.getText().toString();
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:0377778888"));
-               // startActivityForResult(callIntent, 1);
+                callIntent.setData(Uri.parse("tel:"+number));
+                startActivity(callIntent);
             }
         });
 
     }
+
 }
