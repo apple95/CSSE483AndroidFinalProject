44565569c8102d78c4d61c74ec4942503383a6da
apple95 <rajaa@roe-hulman.edu>
2016-08-18 11:18:00 -0400

    Fix

--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity2FixAppointment.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity2FixAppointment.java
@@ -9,12 +9,19 @@ import android.widget.EditText;
 
 import com.firebase.client.Firebase;
 import com.google.android.gms.common.data.BitmapTeleporter;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by ashokvardhanraja on 7/23/16.
  */
 public class Activity2FixAppointment extends Activity {
-    Firebase ref = new Firebase("https://csse483androidfinalprije-63abd.firebaseio.com");
+   // Firebase ref = new Firebase("https://csse483androidfinalprije-63abd.firebaseio.com");
+    private DatabaseReference mDatabase;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
@@ -23,16 +30,54 @@ public class Activity2FixAppointment extends Activity {
         final EditText editText3 = (EditText)findViewById(R.id.mobile);
         final EditText editText4 = (EditText)findViewById(R.id.hospitalVisited);
         Button submitbutton = (Button)findViewById(R.id.register);
+        class blahListener implements ChildEventListener {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                User user  = dataSnapshot.getValue(User.class);
+                user.setKey(dataSnapshot.getKey());
+                //User user = new User(editText1.getText().toString(), editText2.getText().toString(),editText3.getText().toString(),editText4.getText().toString());
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        }
         submitbutton.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
+                mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
+                mDatabase.addChildEventListener(new blahListener());
 
-                Firebase alanRef = ref.child("users").child("blah");
-                User user = new User(editText1.getText().toString(), editText2.getText().toString(),editText3.getText().toString(),editText4.getText().toString());
-                alanRef.setValue(user);
+               // DatabaseReference alanRef = mDatabase.child("users").child("Default");
+               User user = new User(editText1.getText().toString(), editText2.getText().toString(),editText3.getText().toString(),editText4.getText().toString());
+//                alanRef.setValue(user);
+         //       alanRef.child("fullName").push().setValue(editText1.getText().toString());
+                mDatabase.push().setValue(user);
 
             }
         });
 
     }
+
+
 }
--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/User.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/User.java
@@ -1,9 +1,30 @@
 package com.ashokrv95gmail.csse483androidfinalproject;
 
+import com.google.firebase.database.Exclude;
+
 public class User {
     private String ssn;
     private String fullName;
     private String contactNumber;
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public void setHospital(String hospital) {
+        this.hospital = hospital;
+    }
+
+    public void setSsn(String ssn) {
+
+        this.ssn = ssn;
+    }
+
+    private String key;
     private String hospital;
 
     public User() {}
@@ -27,6 +48,22 @@ public class User {
         return hospital;
     }
 
+    @Exclude
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setValues(User user) {
+        this.fullName = user.fullName;
+        this.ssn = user.ssn;
+
+        this.contactNumber = user.contactNumber;
+        this.hospital = user.hospital;
+    }
 
 
 }
