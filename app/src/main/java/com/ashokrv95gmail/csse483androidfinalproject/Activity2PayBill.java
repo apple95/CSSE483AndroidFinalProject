afcaa1816154e468b43c611e4cc0142fc79248f5
apple95 <rajaa@roe-hulman.edu>
2016-08-18 11:32:04 -0400

    Linked to paypal for paying

--- app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity2PayBill.java
+++ app/src/main/java/com/ashokrv95gmail/csse483androidfinalproject/Activity2PayBill.java
@@ -2,6 +2,7 @@ package com.ashokrv95gmail.csse483androidfinalproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,5 +15,15 @@ public class Activity2PayBill extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paybills_view);
+
+        Button pay = (Button)findViewById(R.id.pay);
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.com/signin?country.x=US&locale.x=en_US"));
+                startActivity(intent);
+            }
+        });
+
     }
 }
